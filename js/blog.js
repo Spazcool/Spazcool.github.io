bfa9020dfb8146db6735f30171f62f8fe8cced2b
Doug <douglas.wrightiii@gmail.com>
2017-05-07 15:04:38 +0800

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,9 +1,9 @@
 function main() {
     $(".fcc,.hr,.misc,#home,.projects,.created,#social1,#social2,#social3,#social4,#social5").each(function() {
         $(this).hide();
-
     });
 
+    //OPENING ANIMATION
     $(".fcc").delay(1000).fadeIn(1000);
     $(".hr").delay(2000).fadeIn(1000);
     $(".misc").delay(3000).fadeIn(1000);
@@ -14,8 +14,8 @@ function main() {
     $("#social4").delay(6000).fadeIn(1000);
     $("#social5").delay(6500).fadeIn(1000);
     $("#home").delay(7000).fadeIn(1000);
-
-
+    
+    //SLIDE TOGGLE ANIMATIONS
     $(".projects-button").on("click", function() {
         $(this).toggleClass("active");
         $(this).text("Viewed");
