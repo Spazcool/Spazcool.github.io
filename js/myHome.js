e6caf723c86d32844a0d8c99e621039f7dedfbfd
Doug Wright <douglas.wrightiii@gmail.com>
2018-09-07 16:01:00 -0400

    remove lightning func and ref to blog

--- js/myHome.js
+++ js/myHome.js
@@ -1,28 +1,12 @@
 //ICON FADE IN & SLIDE TOGGLE
 function menu() {
 	$(".menu").hide();
-	$(".blog").hide();	
 	$(".menu").fadeIn(5000);
-	$(".blog").fadeIn(5000);
 	$(".links").hide();
 	$(".nav-button").on("click", function() {
 		$(this).next().slideToggle(400);
 	});
 }
 
-//LIGHTNING ANIMATION
-function lightning() {
-	$(".lightning").hide();
-	$(".title").on("click", function() {
-		for (var i = 0; i < 4; i++) {
-			$(".lightning").fadeIn(80);
-			$(".lightning").fadeOut(130);
-		}
-			$(".lightning").fadeIn(80);
-		$(".lightning").fadeOut(1000);
-	});	
-}
-
 $(window).resize();
 $(document).ready(menu);
-$(document).ready(lightning);
