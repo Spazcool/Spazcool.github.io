425a80db7171d6adb9cb614ff351627634654cb3
Doug Wright <douglas.wrightiii@gmail.com>
2018-09-07 16:27:17 -0400

    remove everything

--- js/myHome.js
+++ js/myHome.js
@@ -1,12 +1,1 @@
-//ICON FADE IN & SLIDE TOGGLE
-function menu() {
-	$(".menu").hide();
-	$(".menu").fadeIn(5000);
-	$(".links").hide();
-	$(".nav-button").on("click", function() {
-		$(this).next().slideToggle(400);
-	});
-}
-
 $(window).resize();
-$(document).ready(menu);
